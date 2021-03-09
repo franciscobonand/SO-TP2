76062cbd4327eefa4cb8061871a3570422e5bf93
Lucas Lopes <lucasclopesr@gmail.com>
2021-03-09 16:30:42 -0300

    Script para gerar experimentos e resultados

--- scripts/experiments.sh
+++ scripts/experiments.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
-for method in lru 2a fifo 
+for file in compilador.log compressor.log  matriz.log  simulador.log
 do
-  for i in 2 4 8 16 32 64
+  for method in lru 2a fifo rand
   do
-    for j in 128 256 512 1024 2048 4096 8192 16384
+    for i in 2 4 8 16 32 64
     do
-      echo "Running $method examples/compilador.log $i $j"
-      ./../out/tp2virtual $method examples/compilador.log $i $j | tr -d '\n' | sed -E "s/.*páginas: ([0-9]+).*memória: ([0-9]+).*Algoritmo: (.*)Reading file (.*)Paginas lidas: ([0-9]+)Paginas escritas: ([0-9]+)Tempo gasto: (.*)Endereço(.*)/\1,\2,\3,\4,\5,\6,\7\n/" >> data/experiments.csv
+      for j in 128 256 512 1024 2048 4096 8192 16384
+      do
+        echo "Running $method examples/$file.log $i $j"
+        ./out/tp2virtual $method examples/$file $i $j | tr -d '\n' | sed -E "s/.*páginas: ([0-9]+).*memória: ([0-9]+).*Algoritmo: (.*)Lendo o arquivo examples\/(.*)Paginas lidas: ([0-9]+)Paginas escritas: ([0-9]+)Tempo gasto: (.*)Endereço(.*)/\1,\2,\3,\4,\5,\6,\7\n/" >> data/experiments.csv
+      done
     done
   done
 done
