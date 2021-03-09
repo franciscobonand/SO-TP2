34d2cf09ac6508c15c2bc96261d925d5a18600d7
Lucas Lopes <lucasclopesr@gmail.com>
2021-03-08 21:13:23 -0300

    Altera scripts para moverem os arquivos para out/

--- compile-debug.sh
+++ compile-debug.sh
@@ -1,4 +1,4 @@
 gcc -c src/main.c src/table.c lib/table.h -g
-cp main.o out/main.o
-cp table.o out/table.o
+mv main.o out/main.o
+mv table.o out/table.o
 gcc -o out/tp2virtuald out/main.o out/table.o
